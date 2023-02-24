dcf9ba69d5a2097f20b32633e7d02105b6346340
juanMiguelCastro <115732335+juanMiguelCastro@users.noreply.github.com>
2023-02-24 14:47:50 -0500

    añadir codigo e instalacion de paquete MysqlConnector

--- Proyecto/Connection.vb
+++ Proyecto/Connection.vb
@@ -1,3 +1,31 @@
-﻿Module Connection
+﻿Imports mySql.Data.MySqlClient
+Imports MySqlConnector
+
+Module Connection
+
+
+
+    Dim con As String = "server=localhost; Port=3306;database=inventario-ue; user id=root;password=;"
+    'Dim con As String = "server=35.198.37.23; Port=3306;database=inventario-ue; user id=ue;password=12345;"
+    'Dim con As String = "server=inventory.mysql.database.azure.com; Port=3306;database=inventario-ue; user id=kel2md;password=Students1."'
+    Public MysqlConex As MySqlConnection = New MySqlConnection(con)
+    Private result As String
+    Private command As New MySqlCommand
+    Private adapter As MySqlDataAdapter
+    Private daTa As DataTable
+    'Abrir Conexión'
+    Sub openConn()
+        Try
+            If MysqlConex.State = 0 Then
+                MysqlConex.Open()
+                MsgBox("Hay Conexión")
+            End If
+        Catch ex As Exception
+            MsgBox(ex)
+        End Try
+    End Sub
+    Sub closeConn()
+        MysqlConex.Close()
+    End Sub
 
 End Module
